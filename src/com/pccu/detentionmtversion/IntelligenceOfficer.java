06d4a02d8125da8d074771ce259ee2c836228999
Kai <A7233801@g.pccu.edu.tw>
2019-12-12 15:05:04 +0800

    2-th commit

--- src/com/pccu/detentionmtversion/IntelligenceOfficer.java
+++ src/com/pccu/detentionmtversion/IntelligenceOfficer.java
@@ -4,10 +4,8 @@ public class IntelligenceOfficer {
 
 	private String officer_att 	= ""; 		// Monitor or Agent
 	private String description 	= ""; 		// Informer Name, such as "Teacher In"
-	
 	public IntelligenceOfficer(String att, String desc) {
 		// TODO Auto-generated constructor stub
-
 		officer_att = att;
 		description = desc;
 	}
